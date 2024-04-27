7a94cbd2c75f5a61eeecb68d6ee2faddf8ebe336
asjadhossainkhan193014073 <80187598+asjadhossainkhan193014073@users.noreply.github.com>
2024-04-28 01:50:19 +0600

    Add files via upload

---
+++ EmployeeManagementSystem.Data/EmployeeManagementContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace EmployeeManagementSystem.Data
+{
+
+    public class EmployeeManagementContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
