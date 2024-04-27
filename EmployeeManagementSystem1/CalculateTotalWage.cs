57a598c103842b119799bf2a3f5b86aa18e50c05
asjadhossainkhan193014073 <80187598+asjadhossainkhan193014073@users.noreply.github.com>
2024-04-28 01:31:19 +0600

    Add files via upload

---
+++ EmployeeManagementSystem1/CalculateTotalWage.cs
@@ -0,0 +1,23 @@
+﻿namespace EmployeeManagementSystem1
+{
+
+
+    class CalculateTotalWage
+    {
+        public string EmpName;
+        public string EmpDepartment;
+        public int EmpWageRate;
+        public int EmpWorkedHours;
+        public int EmpTotalWage;
+
+
+        public CalculateTotalWage(string name, string department, int rate, int workedHour)
+        {
+            this.EmpName = name;
+            this.EmpDepartment = department;
+            this.EmpWageRate = rate;
+            this.EmpWorkedHours = workedHour;
+            this.EmpTotalWage = workedHour * rate;
+        }
+    }
+}
